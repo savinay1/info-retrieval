653767149b64b085ef55c9bb0a9cf2c15d6ee24d
savinay1 <savinaysav@gmail.com>
2022-02-19 01:26:24 -0600

    Comments+Replies

--- scrape.py
+++ scrape.py
@@ -1,7 +1,6 @@
 # Read-only instance
 import praw as praw
 import re
-
 import pandas as pd
 import time
 
@@ -17,29 +16,54 @@ reddit_authorized = praw.Reddit(client_id="SvCJdstpGCed5uhK7XJyIw",  # your clie
                                 username="AnyAcanthocephala173",  # your reddit username
                                 password="Beyblade123@")
 
-
-subreddit = reddit_read_only.subreddit("netflix")
-timestart=time.time()
 #
-posts = subreddit.top(limit=50000)
+# posted_after = int(datetime.datetime(2009, 1, 1).timestamp())
+# posted_before = int(datetime.datetime(2021, 12,30 ).timestamp())
 
+#posts = psraw.submission_search(reddit_authorized,subreddit="TrueFilm",limit=None)
+subreddit = reddit_read_only.subreddit("TrueFilm")
+subreddit2 = reddit_read_only.subreddit("MovieSuggestions")
+subreddit3= reddit_read_only.subreddit("movies")
+
+postsl=[]
+posts = subreddit.top(limit=None)
+postsl.append(posts)
+postsl.append(subreddit2.top(limit=None))
+postsl.append(subreddit3.top(limit=None))
 posts_dict = {"Post Text":[],"Comments": [] }
+posts_replies = {"Comments":[],"Replies": [] }
+for posts in postsl:
+    for post in posts:
+        post=reddit_read_only.submission(id=post.id)
+        post.comments.replace_more(limit=0)
+        if len(post.comments) < 10:
+            continue
+        count=0
+        for commentid,comment in enumerate(post.comments.list()):
+            posts_dict["Post Text"].append(re.sub("&.*"," ",post.selftext))
+            posts_dict["Comments"].append(re.sub("&.*"," ",comment.body))
+            title = post.title.replace("/", " ")
+            if len(comment.replies) >0:
+                for replyid,reply in enumerate(comment.replies):
+                    posts_replies["Comments"].append(re.sub("&.*", " ", comment.body))
+                    posts_replies["Replies"].append(re.sub("&.*", " ", reply.body))
+                    if (replyid + 1) % 10 == 0:
+                        top_posts = pd.DataFrame(posts_replies)
+                        top_posts.to_csv("csv/" + title[0:min(len(title), 50)] + "Replies"+str(replyid + 1) + ".csv", index=True)
+                        posts_replies["Comments"] = []
+                        posts_replies["Replies"] = []
+                top_posts = pd.DataFrame(posts_replies)
+                top_posts.to_csv("csv/" + title[0:min(len(title), 50)] + "Replies" + str(replyid + 1) + ".csv", index=True)
+                posts_replies["Comments"] = []
+                posts_replies["Replies"] = []
 
-for post in posts:
-    post=reddit_read_only.submission(id=post.id)
-    post.comments.replace_more(limit=0)
-    if len(post.comments) < 100:
-        continue
-    for commentid,comment in enumerate(post.comments):
-        posts_dict["Post Text"].append(re.sub("&.*"," ",post.selftext))
-        posts_dict["Comments"].append(re.sub("&.*"," ",comment.body))
-        title = post.title.replace("/", " ")
-        if (commentid+1)%10==0:
-            top_posts = pd.DataFrame(posts_dict)
-            top_posts.to_csv("csv/" + title[0:min(len(title), 50)] + str(commentid+1) + ".csv", index=True)
-            posts_dict["Post Text"] = []
-            posts_dict["Comments"] = []
-
+            if (commentid+1)%10==0:
+                top_posts = pd.DataFrame(posts_dict)
+                top_posts.to_csv("csv/" + title[0:min(len(title), 50)] + str(commentid+1) + ".csv", index=True)
+                posts_dict["Post Text"] = []
+                posts_dict["Comments"] = []
+        top_posts.to_csv("csv/" + title[0:min(len(title), 50)] + str(commentid + 1) + ".csv", index=True)
+        posts_dict["Post Text"] = []
+        posts_dict["Comments"] = []
 
 timednd=time.time()
-print(timednd-timestart)
